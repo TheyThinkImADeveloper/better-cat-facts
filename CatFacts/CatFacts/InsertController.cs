0af75a8a52007e1aeebbe8d58fa7229ee9961e95
malenso <mfensor@gmail.com>
2016-11-10 13:07:06 -0600

    Inheritance Example
    Examples of inheritance by Thomas

---
+++ CatFacts/CatFacts/InsertController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatFacts
+{
+    public class Employee
+    {
+
+        public int Fred { get; set; }
+
+        public virtual float CalculatePay()
+        {
+            return 0;
+        }
+
+    }
+
+    public class HourEmployee : Employee
+    {
+        public override float CalculatePay()
+        {
+            return 5 * .23f;
+        }
+    }
+
+    public class SalaryEmployee : Employee
+    {
+        public override float CalculatePay()
+        {
+            return base.CalculatePay();
+        }
+    }
+
+
+    public class ConsumingClass
+    {
+        public ConsumingClass(Employee controller)
+        {
+            controller.CalculatePay();
+        }
+    }
+
+    public class ConsumingConsumingClass
+    {
+        public ConsumingConsumingClass()
+        {
+            new ConsumingClass(new HourEmployee()); //
+            new ConsumingClass(new Employee()); //
+            new ConsumingClass(new SalaryEmployee()) //
+        }
+    }
+}
