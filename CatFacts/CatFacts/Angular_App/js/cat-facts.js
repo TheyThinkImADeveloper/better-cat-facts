c83194138402d15bad2128df891d18e081f4e39e
malenso <mfensor@gmail.com>
2016-12-01 12:41:17 -0600

    Display cat facts in ui using angular and discovered new cat fact

--- CatFacts/CatFacts/Angular_App/js/cat-facts.js
+++ CatFacts/CatFacts/Angular_App/js/cat-facts.js
@@ -1,1 +1,24 @@
-﻿var catApp = angular.module("cats", [])
+﻿var catApp = angular.module("catApp", []);
+
+catApp.controller("CatAppCtrl", ["$scope", "$http", function ($scope, $http) {
+
+    function constructor() {
+        $scope.GetCatFacts();
+    }
+
+    $scope.title = "Hallo";
+    $scope.GetCatFacts = function() {
+        $http({
+            method: 'GET',
+            url: '/api/catfact'
+        }).then(function (response) {
+            console.log(response);
+            $scope.catFacts = response.data;
+        });
+    };
+
+    constructor();
+
+}]);
+
+
