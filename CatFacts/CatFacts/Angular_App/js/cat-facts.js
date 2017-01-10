8e5d8ee4641ce58caa26f94baf1877cf92e23a60
Thomas Sobieck <thomas@sobieck.com>
2017-01-10 13:05:05 -0600

    Almost done with add cat fact!

--- CatFacts/CatFacts/Angular_App/js/cat-facts.js
+++ CatFacts/CatFacts/Angular_App/js/cat-facts.js
@@ -16,17 +16,46 @@ catApp.controller("CatAppCtrl", ["$scope", "$http", function ($scope, $http) {
             $scope.catFacts = response.data;
         });
     };
+
     $scope.InitializeDelete = function ($index) {
         $http({
             method: 'GET',
             url: '/api/catfact/' + $index
         }).then(function (response) {
             console.log(response);
-            $scope.catFacts = response.data;
+            $scope.catFactToDelete = {
+                fact: response.data,
+                index: $index
+            };
+            $scope.showDeletePrompt = true;
+            $scope.showMessageWasDeleted = false;
         });
     }
-    $scope.ConfirmDelete = function () {
 
+    $scope.ConfirmDelete = function ($index) {
+        $http({
+            method: 'DELETE',
+            url: '/api/catfact/' + $index
+        }).then(function () {
+            $scope.showMessageWasDeleted = true;
+            $scope.GetCatFacts();
+            $scope.showDeletePrompt = false;
+        });
+    }
+
+    $scope.CancelDelete = function() {
+        $scope.showDeletePrompt = false;
+    }
+
+    $scope.AddCatFact = function(newFact) {
+        $http({
+            data: newFact,
+            method: 'POST',
+            url: '/api/catfact/'
+    }).then(function () {
+            console.log();
+            $scope.GetCatFacts();
+        });
     }
 
     constructor();
